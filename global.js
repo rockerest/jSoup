69e9e85438cab026934112f1149e917f8ffd1e53
Tom Randolph <websites@thomasrandolph.info>
2013-01-15 13:47:25 -0600

    Smart namespace parsing in storage engine
    Added namespace parsing for store/retrieve and better empty checking.

--- global.js
+++ global.js
@@ -1,16 +1,24 @@
 // Utilities
 (function( Utils, undefined ){
     "use strict";
-    // namespace function
-    //adapted from http://addyosmani.com/blog/essential-js-namespacing/
-    Utils.namespace = function( namespace ){
-        var parts = namespace.split("."),
-            parent = window,
-            pl, i;
+
+    Utils.parseNamespace = function( namespace ){
+        var parts = namespace.split(".");
 
         if( parts[0] == "window" ){
             parts = parts.slice( 1 );
         }
+
+        return parts;
+    };
+
+    // namespace function
+    //adapted from http://addyosmani.com/blog/essential-js-namespacing/
+    Utils.namespace = function( namespace ){
+        var parent = window,
+            pl, i, parts;
+
+        parts = Utils.parseNamespace( namespace );
 
         pl = parts.length;
         for( i = 0; i < pl; i++ ){
@@ -60,28 +68,28 @@
         return bool;
     };
 
-	Utils.recursiveObjectMerge = function( primary, overwrite ){
+    Utils.recursiveObjectMerge = function( primary, overwrite ){
         var p;
-		for( p in overwrite ){
-			try{
-				//try an update
-				if( overwrite[p].constructor == Object ){
-					primary[p] = Utils.recursiveObjectMerge( primary[p], overwrite[p] );
-				}
-				else{
-					primary[p] = overwrite[p];
-				}
-			}
-			catch( e ){
-				// destination doesn't have that property, create and set it
-				primary[p] = overwrite[p];
-			}
-		}
+        for( p in overwrite ){
+            try{
+                //try an update
+                if( overwrite[p].constructor == Object ){
+                    primary[p] = Utils.recursiveObjectMerge( primary[p], overwrite[p] );
+                }
+                else{
+                    primary[p] = overwrite[p];
+                }
+            }
+            catch( e ){
+                // destination doesn't have that property, create and set it
+                primary[p] = overwrite[p];
+            }
+        }
 
-		// primary is modified (it's a reference), but pass it back
-		// to keep up the idea that this function returns a result
-		return primary;
-	};
+        // primary is modified (it's a reference), but pass it back
+        // to keep up the idea that this function returns a result
+        return primary;
+    };
 
     Utils.merge = function( one, two ){
         return Utils.recursiveObjectMerge( Utils.recursiveObjectMerge( {}, one ), two );
@@ -91,59 +99,17 @@
         return Utils.merge( {}, object );
     };
 
-    Utils.empty = function( v ){
-        return (v === undefined || v == "" || v == null);
+    Utils.empty = function( obj ){
+        if (typeof obj == 'undefined' || obj === null || obj === '') return true;
+        if (typeof obj == 'number' && isNaN(obj)) return true;
+        if (obj instanceof Date && isNaN(Number(obj))) return true;
+        return false;
     };
 
 }( window.Utils = window.Utils || {} ));
 
 // Browser Wrappers
 (function( Browser, undefined ){
-    // define a single Cookie
-    Browser.Cookie = function( name, value, settings ){
-        this.defaults = {
-            expire:     0,      // defaults to session
-            path:       "/",    // defaults to the root of the web server
-            domain:     null,   // no default domain
-            secure:     false   // defaults to not secure
-        };
-
-        if( settings === undefined || !( settings instanceof Object ) ){
-            settings = {};
-        }
-
-        this.settings = Utils.merge( this.defaults, settings );
-
-        this.name = name;
-        this.value = value;
-
-        this.toString = function(){
-            var selfString = this.name + "=" + escape( this.value );
-
-            // set the expiration
-            if( this.settings.expire != 0 && this.settings.expire instanceof Date ){
-                selfString += "; expires=" + this.settings.expire.toUTCString();
-            }
-
-            // set the path
-            if( this.settings.path != null && this.settings.path != "" ){
-                selfString += "; path=" + escape( this.settings.path );
-            }
-
-            // set the domain
-            if( this.settings.domain != null && this.settings.domain != "" ){
-                selfString += "; domain=" + escape( this.settings.domain );
-            }
-
-            // set whether secure or not
-            if( this.settings.secure ){
-                selfString += "; secure";
-            }
-
-            return selfString;
-        };
-    };
-
 	// set up the Cookie jar
 	(function( Cookies, undefined ){
 		Cookies.cookies = document.cookie;
@@ -154,56 +120,121 @@
 				Cookies.remove( cookie.name );
 			}
 
-            document.cookie = cookie.toString();
-        };
+			document.cookie = cookie.toString();
+		};
 
-        Cookies.get = function( name ){
-            var results = Cookies.cookies.match( "(^|;) ?" + name + "=([^;]*)(;|$)" );
+		Cookies.get = function( name ){
+			var results = Cookies.cookies.match( "(^|;) ?" + name + "=([^;]*)(;|$)" );
 
-            if( results ){
-                return unescape( results[2] );
-            }
-            else{
-                return null;
-            }
-        };
+			if( results ){
+				return unescape( results[2] );
+			}
+			else{
+				return null;
+			}
+		};
 
-        Cookies.remove = function( name ){
-            var past = new Date();
-            past.setTime( past.getTime() - 10101010 );
+		Cookies.remove = function( name ){
+			var past = new Date();
+			past.setTime( past.getTime() - 10101010 );
 
-            document.cookie = name += "=; expires=" + past.toUTCString();
-        };
-    }( Browser.Cookies = Browser.Cookies || {} ));
+			document.cookie = name += "=; expires=" + past.toUTCString();
+		};
+	}( Browser.Cookies = Browser.Cookies || {} ));
 
     (function( Storage, undefined ){
         Storage.init = function(){
             if( typeof Storage.__datastore != 'object' ){
                 Storage.__datastore = {};
             }
-        }
+        };
 
         Storage.store = function( key, value ){
+            var data,parent,pl,i,parts;
+
             Storage.init();
-            var data = Storage.retrieve();
-            if( typeof data == 'undefined' ){
-                data = {};
+            data = Storage.retrieve();
+            parent = Storage.__datastore;
+
+            Utils.namespace( "Browser.Storage.__datastore." + key );
+            parts = Utils.parseNamespace( key );
+
+            pl = parts.length - 1;
+            for( i = 0; i < pl; i++ ){
+                parent = parent[parts[i]];
             }
 
-            data[key] = value;
-            Storage.__datastore = data;
-        }
+            parent[ parts[ i ] ] = value;
+        };
 
         Storage.retrieve = function( key ){
+            var parent = window.Browser.Storage.__datastore,
+                data, pl, i, parts, el;
             Storage.init();
-            if( typeof key == "undefined" ){
-                return Storage.__datastore;
+
+            if( key === undefined ){
+                data = Storage.__datastore;
             }
             else{
-                return Storage.__datastore[ key ];
+                parts = Utils.parseNamespace( key );
+
+                pl = parts.length;
+                for( i = 0; i < pl; i++ ){
+                    parent = parent[parts[i]];
+                }
+
+                data = parent;
             }
-        }
-    }( Browser.Storage = Browser.Storage || {} ))
+
+            return data;
+        };
+
+    }( Browser.Storage = Browser.Storage || {} ));
+
+	// define a single Cookie
+	Browser.Cookie = function( name, value, settings ){
+		this.defaults = {
+			expire:		0,		// defaults to session
+			path:		"/",	// defaults to the root of the web server
+			domain:		null,	// no default domain
+			secure:		false	// defaults to not secure
+		};
+
+		if( settings === undefined || !( settings instanceof Object ) ){
+			settings = {};
+		}
+
+		this.settings = Utils.merge( this.defaults, settings );
+
+		this.name = name;
+		this.value = value;
+
+		this.toString = function(){
+			var selfString = this.name + "=" + escape( this.value );
+
+			// set the expiration
+			if( this.settings.expire != 0 && this.settings.expire instanceof Date ){
+				selfString += "; expires=" + this.settings.expire.toUTCString();
+			}
+
+			// set the path
+			if( this.settings.path != null && this.settings.path != "" ){
+				selfString += "; path=" + escape( this.settings.path );
+			}
+
+			// set the domain
+			if( this.settings.domain != null && this.settings.domain != "" ){
+				selfString += "; domain=" + escape( this.settings.domain );
+			}
+
+			// set whether secure or not
+			if( this.settings.secure ){
+				selfString += "; secure";
+			}
+
+			return selfString;
+		};
+	};
 
     // Navigation helper
     Browser.go = function(loc){
